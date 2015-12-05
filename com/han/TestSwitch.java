62f8aff01efcbc52d4b9d4868634cf23cfbb07d2
Han shenquan <1173956412@qq.com>
2015-12-05 20:43:02 +0800

    test1

---
+++ com/han/TestSwitch.java
@@ -0,0 +1,18 @@
+package com.han;
+
+/**
+ * Created by HSQ on 2015/12/5.
+ */
+public class TestSwitch {
+    public static void main(String [] args){
+        int t=2;
+//        int c;
+        switch (t){
+            case 1:System.out.println(1);
+            case 2:System.out.println(2);
+            case 3:System.out.println(3);
+            default:System.out.println("default");
+
+        }
+    }
+}
